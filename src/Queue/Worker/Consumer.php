599edbfdf6d1515e3818835b03a8ded802ff44eb
Nayleen <almightynay@gmail.com>
2023-12-03 23:43:18 +0100

    chore: add missing bus queue worker

---
+++ src/Queue/Worker/Consumer.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Nayleen\Async\Bus\Queue\Worker;
+
+use Amp\Cancellation;
+use Nayleen\Async\Bus\Queue\Consumer as QueueConsumer;
+use Nayleen\Async\Bus\Queue\Queue;
+use Nayleen\Async\Timers;
+use Nayleen\Async\Worker;
+
+class Consumer extends Worker
+{
+    public function __construct(
+        private readonly QueueConsumer $consumer,
+        private readonly Queue $queue,
+        Timers $timers = new Timers(),
+    ) {
+        parent::__construct($timers);
+    }
+
+    protected function execute(Cancellation $cancellation): void
+    {
+        $this->consumer->consume($this->queue, $cancellation);
+    }
+}
---
+++ tests/Unit/Queue/Worker/ConsumerTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Nayleen\Async\Bus\Queue\Worker;
+
+use Amp\PHPUnit\AsyncTestCase;
+use Nayleen\Async\Bus\Queue\Consumer as QueueConsumer;
+use Nayleen\Async\Bus\Queue\Queue;
+use Nayleen\Async\Test\TestKernel;
+use Nayleen\Async\Worker\Queue\Consumer;
+
+/**
+ * @internal
+ */
+final class ConsumerTest extends AsyncTestCase
+{
+    /**
+     * @test
+     */
+    public function can_run(): void
+    {
+        $kernel = TestKernel::create();
+        $queue = $this->createStub(Queue::class);
+
+        $queueConsumer = $this->createMock(QueueConsumer::class);
+        $queueConsumer->expects(self::once())->method('consume')->with($queue);
+
+        $worker = new Consumer($queueConsumer, $queue);
+        $worker->run($kernel);
+    }
+}
